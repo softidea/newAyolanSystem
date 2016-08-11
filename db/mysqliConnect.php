faa39ff1dbb5924e8b0485e7e809da8df23b1890
malin <malin@malinda>
2016-08-11 08:25:56 +0530

    new system changed

--- db/mysqliConnect.php
+++ db/mysqliConnect.php
@@ -4,18 +4,17 @@ if (!isset($_SESSION['user_email'])) {
   header("Location:../index.php");  
 }
 
-//
-//define('db_host', '77.104.142.97');
-//define('db_port', '3306');
-//define('db_user', 'ayolanin_dev');
-//define('db_password', 'WelComeDB1129');
-//define('db_database', 'ayolanin_datahost');
-
-define('db_host', 'localhost');
+define('db_host', '77.104.142.97');
 define('db_port', '3306');
-define('db_user', 'root');
-define('db_password', '1234');
+define('db_user', 'ayolanin_dev');
+define('db_password', 'WelComeDB1129');
 define('db_database', 'ayolanin_datahost');
+
+//define('db_host', 'localhost');
+//define('db_port', '3306');
+//define('db_user', 'root');
+//define('db_password', '1234');
+//define('db_database', 'ayolanin_datahost');
 
 $d_bc = mysqli_connect(db_host, db_user, db_password, db_database)
 
--- db/newDB.php
+++ db/newDB.php
@@ -1,7 +1,7 @@
 <?php
 
-//$conn = mysqli_connect("77.104.142.97", "ayolanin_dev", "WelComeDB1129", "ayolanin_datahost");
-$conn = mysqli_connect("localhost", "root", "1234", "ayolanin_datahost");
+$conn = mysqli_connect("77.104.142.97", "ayolanin_dev", "WelComeDB1129", "ayolanin_datahost");
+//$conn = mysqli_connect("localhost", "root", "1234", "ayolanin_datahost");
 if (mysqli_connect_errno()) {
     echo "Falied to Connect the Database" . mysqli_connect_error();
 }
