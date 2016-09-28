1eb12afe1c07e24027685d33caf9b3ef5e2cc4f8
deepalsuranga <deepalsuranga@live.com>
2016-09-29 00:36:40 +0530

    privilage managment is over 98%-ds admin 5% block-no dineth-kela_head

--- admin/admin_home.php
+++ admin/admin_home.php
@@ -2,8 +2,7 @@
 session_start();
 
 if (!isset($_SESSION['user_email'])) {
-    header("Location:../../"
-            . "index.php");
+    header("Location:../../index.php");
 }
 ?>
 <!DOCTYPE html>
